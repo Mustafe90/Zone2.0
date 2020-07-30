24e9f8d7405954995d27b3a565760a12846e7935
Mustafe Farhan <mustafe.farhan@method4.co.uk>
2020-07-31 00:02:08 +0100

    initial share functionality in the controller.

--- Zone.Website/Controllers/SpotifyController.cs
+++ Zone.Website/Controllers/SpotifyController.cs
@@ -12,10 +12,12 @@ namespace Zone.Website.Controllers
     public class SpotifyController : Controller
     {
         private readonly SpotifyClientDomain _spotifyClient;
+        private readonly ShareClientDomain _clientDomain;
 
-        public SpotifyController(SpotifyClientDomain spotifyHttpClientService)
+        public SpotifyController(SpotifyClientDomain spotifyHttpClientService, ShareClientDomain clientDomain)
         {
             _spotifyClient = spotifyHttpClientService;
+            _clientDomain = clientDomain;
         }
         public async Task<IActionResult> Library()
         {
@@ -24,12 +26,16 @@ namespace Zone.Website.Controllers
             return View(albums);
         }
         [HttpPost]
-        public IActionResult Share([FromForm] IEnumerable<ShareAlbum> album)
+        public IActionResult Share([FromForm] ICollection<ShareAlbum> album)
         {
             if (!ModelState.IsValid)
             {
+                //Todo: Implement an error page that is helpful
                 return null;
             }
+
+            _clientDomain.ShareAlbums(album);
+            _clientDomain.ShareTracks(album);
 
             return null;
         }
